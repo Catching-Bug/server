33ddab07bb548845a2cfd284008fea0b714dd03c
Cha-Young-Ho <jkijki1249@gmail.com>
2022-08-12 16:06:08 +0900

    Feature : Locations 조회 기능 추가

--- src/main/java/com/catchbug/server/location/LocationController.java
+++ src/main/java/com/catchbug/server/location/LocationController.java
@@ -1,7 +1,16 @@
 package com.catchbug.server.location;
 
+import com.catchbug.server.jwt.model.AuthUser;
+import com.catchbug.server.location.dto.DtoOfCreateLocation;
+import com.catchbug.server.member.dto.DtoOfGetLocation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * <h1>LocationController</h1>
  * <p>
@@ -19,5 +28,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class LocationController {
     private final LocationService locationService;
 
+    /**
+     * 요청한 회원의 사전 등록한 위치 정보들을 조회한다.
+     * @param authUser : 요쳥한 유저
+     * @return 요청한 유저가 사전등록한 위치 정보 객체 리스트 dto
+     */
+    @GetMapping("/api/locations")
+    public ResponseEntity<?> getLocations(AuthUser authUser){
+        List<DtoOfGetLocation> locationList = locationService.getLocations(Long.parseLong(authUser.getId()));
+
+        return ResponseEntity.ok().body(locationList);
+    }
 
 }
